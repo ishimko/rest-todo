c33bcb32a82d7c098f03c9189f65579ee3fa50f8
Ivan <vana.shimko@gmail.com>
2016-05-10 18:27:56 +0300

    getting all tasks, getting task  by id

--- rest_handler.py
+++ rest_handler.py
@@ -1,14 +1,49 @@
 import http_handler
 from http import HTTPStatus
+import json
+
 from logger import log
 
 
 class RESTHandler(http_handler.HTTPRequestHandler):
-    def do_HEAD(self):
-        self.send_response(HTTPStatus.OK)
+    tasks = [
+        {
+            'id': 1,
+            'title': 'Do course work',
+            'description': 'oops',
+            'done': False
+        },
+        {
+            'id': 2,
+            'title': 'Do OOP',
+            'description': 'Lab 4, 5',
+            'done': False
+        }
+
+    ]
 
     def do_GET(self):
-        self.send_response(HTTPStatus.OK, 'GET')
+        segments = self.path.split('/')
+        method = segments[1]
+        if method == 'todo':
+            if len(segments) == 3:
+                try:
+                    task_id = int(segments[2])
+                except ValueError:
+                    self.send_error(HTTPStatus.NOT_FOUND)
+                    return
+                self.get_task(task_id)
+            else:
+                self.get_all_tasks()
+        else:
+            self.send_error(HTTPStatus.NOT_FOUND)
+
+    def _send_json(self, object):
+        self.send_response(HTTPStatus.OK, json.dumps(object, indent=True))
+
+    def get_all_tasks(self):
+        log('sending all tasks')
+        self._send_json(self.tasks)
 
     def do_POST(self):
         self.send_response(HTTPStatus.OK, 'POST')
@@ -29,3 +64,13 @@ class RESTHandler(http_handler.HTTPRequestHandler):
         if content is not None:
             content = content.encode('UTF-8', 'replace')
             self.wfile.write(content)
+
+    def get_task(self, task_id):
+        task = [x for x in self.tasks if x['id'] == task_id]
+
+        if not task:
+            self.send_error(HTTPStatus.NOT_FOUND)
+            log('task {} not found', format(task_id))
+        else:
+            log('sending task {}'.format(task_id))
+            self._send_json(task[0])
